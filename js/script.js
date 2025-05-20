a91232f3bbdd66c647fb9648d3684bf2b42e5246
Mohammad <mohammad.dahamshi@gmail.com>
2025-05-20 17:34:40 +0300

    Implements playGame, playRound and other helper functions.
    Implements remaining functions with help function getResult that returns one from three:
    - 'win
    - 'lose'
    - 'draw'
    this function use a lookup table for fast result compute withput if/else.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,76 @@
 const RPSGame = {
+    choices_idx: {
+        paper: 1, 
+        rock: 0, 
+        scissors: 2
+    },
+    turns: 5,
+    winnerMat : {
+        rock: {scissors: 'win', paper: 'lose', rock: 'draw'},
+        paper: {rock: 'win', scissors: 'lose', paper: 'draw'},
+        scissors: {paper: 'win', rock: 'lose', scissors: 'draw'}
+    },
+    
     choices: ['rock', 'paper', 'scissors'],
+    humanScore: 0, computerScore: 0,
+    
     getComputerChoice(){
         let rand = Math.floor(Math.random() * 3);
         return this.choices[rand];
     },
+
     getHumanChoice(){
         const userIn = prompt('Choose paper, rock, scissors').toLowerCase().trim();
-        if(! this.choices.includes(userIn)){
-            alert('Invalid input');
-            return;   
-        }
+
         return userIn;
+    },
+
+    getResult(human, computer){
+        return this.winnerMat[human][computer];
+    },
+
+    playRound(humanChoice, computerChoice){
+
+        humanChoice = humanChoice.toLowerCase();
+        if(! this.choices.includes(humanChoice)){
+            console.error(`${humanChoice} is not a good input !`)
+            return;
+        }
+        let res = this.getResult(humanChoice, computerChoice);
+        if(res == 'win'){
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            this.humanScore++;
+        }
+        if(res == 'lose'){
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            this.computerScore++;
+        }
+        if(res == 'draw')
+            console.log(`Draw! both choosed ${humanChoice}`);
+        return;
+    },
+
+    playGame(){
+        this.humanScore = 0;
+        this.computerScore = 0;
+
+        for(let i = 0; i < this.turns; i++){
+            let humanChoice = this.getHumanChoice();
+            let computerChoice = this.getComputerChoice();
+            this.playRound(humanChoice, computerChoice);
+        }
+        if(this.humanScore > this.computerScore)
+            console.log(`You won! ${this.humanScore} - ${this.computerScore}.`);
+
+        else if(this.humanScore < this.computerScore)
+            console.log(`You lose! ${this.humanScore} - ${this.computerScore}.`);
+
+        else
+            console.log(`Draw! ${this.humanScore} - ${this.computerScore}.`);
+        return;
     },
 };
 
 exports = {
     RPSGame
-}
+};
